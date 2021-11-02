a7dfe02c33c9826fbe91c357d614dd676ba7ea3d
Alan <jiuyang.zhang@rakuten.com>
2021-11-02 16:52:04 +0900

    add a test case for new route

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,4 +26,11 @@ public class HelloControllerTest {
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
+
+	@Test
+	public void getWorld() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/world").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("The world is yours to explore!")));
+	}
 }
